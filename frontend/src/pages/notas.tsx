50125845d1d20ff52215701f35bec91d611bb6ab
Julie Moura <juliemouradev@gmail.com>
2023-03-23 01:45:32 -0300

    feat: add notas page

---
+++ frontend/src/pages/notas.tsx
@@ -0,0 +1,10 @@
+import { Container, Subtitle, Title } from '@/styles/home/styles';
+import React from 'react';
+
+const Notas = () => {
+    return (
+        <h4>In development!</h4>
+    );
+};
+
+export default Notas;
